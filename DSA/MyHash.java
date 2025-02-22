cbdbf272b97dc1294375ec9cb5f1bffc606beed3
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-02-22 11:29:42 +0530

    Update

---
+++ DSA/MyHash.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class MyHash {
+    public static void main(String[] args) {
+        HashMap<String,Integer>map = new HashMap<>();
+        map.put("Potato",2);
+        map.put("Brocoli",1);
+        System.out.println(map);
+
+
+
+    }
+}

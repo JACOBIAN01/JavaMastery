e8c9750a08a1b6d08ebc6c477a4c9976e5cfcdb0
JACOBIAN01 <subhadeep23@gmail.com>
2025-03-29 02:49:48 +0530

    Update

--- CAT/LL3.java
+++ CAT/LL3.java
@@ -2,4 +2,33 @@ package CAT;
 
 public class LL3 {
     
+    static class Node{
+        int data;
+        Node next;
+        Node(int d){
+            this.data = d;
+            this.next = null;
+        }
+    }
+    static Node head;
+
+    static void push(int newData){
+        Node newNode = new Node(newData);
+        newNode.next= head;
+        head=newNode;
+    }
+
+    void DetectLoop(){
+        Node slow = head ; Node Fast = head;
+        while(slow!=null && Fast !=null && fast.next != null){
+            slow = slow.next;
+            Fast = Fast.next.next;
+            if(slow==Fast){
+                System.out.println("Loop FOund");
+                return;
+            }
+        }
+        System.out.println("Loop Not Found");
+    }
+
 }
---
+++ CAT/SortBitonicDLL.java
@@ -0,0 +1,20 @@
+// package CAT;
+
+// public class SortBitonicDLL {
+    
+//     static class Node{
+//         int data;
+//         Node next;
+//         Node prev;
+//     }
+
+//     static Node reverse(Node head_ref){
+//         Node temp = null;
+//         Node current = head_ref;
+
+//         while(temp!=current){
+//             temp = current.prev;
+//         }
+//     }
+
+// }

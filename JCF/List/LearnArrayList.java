0a4906bdd513621a647c695aaba71492b5a57113
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-11-11 14:55:34 +0530

    Update ArrayList

--- JCF/List/LearnArrayList.java
+++ JCF/List/LearnArrayList.java
@@ -1,7 +1,48 @@
+import java.util.*;
 
 public class LearnArrayList {
-
     public static void main(String[] args) {
-        System.out.println("hi");
+    
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+        list.add("Alice");
+        list.add("Bob");
+        list.add(1,"Subhadeep");
+
+        System.out.println(list.get(2));
+        System.out.println(list);
+
+        list.set(0,"Alicia");
+        System.out.println(list);
+
+        list.remove(2);
+        list.remove("Subhadeep");
+        System.out.println(list);
+
+        System.out.println(list.size());
+
+        System.out.println(list.contains("Subhadeep"));
+
+        for(String s:list){System.out.println(s);}
+
+        // String[] arr = list.toArray(new String[0]);
+        // for(String s:arr){System.out.println(s);}
+
+        list2.add(1);
+        list2.add(2);
+        list2.add(3);
+        list2.add(4);
+        list2.add(5);
+
+        for(int i:list2){System.out.println(i);}
+        Iterator<Integer> i = list2.iterator();
+        while(i.hasNext()){
+            System.out.println(i);
+        }
+
+
+
+
     }
 }
